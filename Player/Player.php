bb7a53abadd726c6bdd6f1ebf1d33c94af971ada
Kennedy Miyake <kennedykaiak57@gmail.com>
2024-11-24 19:32:14 -0300

    feat: adicionado o construct na classe Player

--- Player/Player.php
+++ Player/Player.php
@@ -4,6 +4,10 @@ class Player {
     private string $nickname;
     private int $nivel;
     private Inventario $inventario;
+
+    public function __construct(string $nickname) {
+        $this->getNickname($nickname);
+    }
 }
 
 ?>
