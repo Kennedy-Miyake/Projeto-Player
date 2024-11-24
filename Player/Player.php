dd7b9bbdf7300412376dd9f6f1c253003256ca2e
Kennedy Miyake <kennedykaiak57@gmail.com>
2024-11-24 19:51:54 -0300

    feat: adicionado todos os sets da classe Player

--- Player/Player.php
+++ Player/Player.php
@@ -6,7 +6,14 @@ class Player {
     private Inventario $inventario;
 
     public function __construct(string $nickname) {
-        $this->getNickname($nickname);
+        $this->setNickname($nickname);
+    }
+
+    public function setNickname(string $nickname): void {
+        $this->nickname = $nickname;
+    }
+    public function setNivel(int $nivel): void {
+        $this->nivel = $nivel;
     }
 }
 
