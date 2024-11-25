a283a5b68afd88f4d542c0261b1152d74a3d98e1
Raul <raulfabiano.dev@gmail.com>
2024-11-24 22:40:03 -0300

    adicionado o construct na classe item

--- Item/Item.php
+++ Item/Item.php
@@ -4,6 +4,11 @@ class Item {
     private string $name;
     private int $tamanho;
     private string $classe;
-}
 
+    public function __construct(string $name, int $tamanho, string $classe) {
+        $this->getName($name);
+        $this->getTamanho($tamanho);
+        $this->getclasse($classe);
+    }
+}
 ?>
