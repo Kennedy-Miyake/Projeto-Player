03967733810dfeb528f018175747c44e597e5490
Raul <raulfabiano.dev@gmail.com>
2024-11-24 22:51:43 -0300

    adicionando os sets da classe item

--- Item/Item.php
+++ Item/Item.php
@@ -10,15 +10,24 @@ class Item {
         $this->getTamanho($tamanho);
         $this->getclasse($classe);
     }
-    
+
     public function getName(): string {
         return $this->name;
+    }
+    public function  setName(string $name): void {
+        $this->name = $name;
     }
     public function getTamanho(): int {
         return $this->tamanho;
     }
+    public function setTamanho(int $tamanho): void {
+        $this->tamanho = $tamanho;
+    }
     public function getClasse(): string {
         return $this->classe;
+    }
+    public function setClasse(string $classe): void {
+        $this->classe = $classe;
     }
     
  
