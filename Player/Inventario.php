1802347e368b00eb950d17d71168d99416a3aa86
takiguchi <alexandre.pupim360@gmail.com>
2024-11-29 13:49:01 -0300

    Modificando Função adicionar do 'inventario.php'
    Corrigindo o erro da função adicionar no 'inventario.php'

--- Player/Inventario.php
+++ Player/Inventario.php
@@ -1,21 +1,31 @@
 <?php 
 
-require_once("Item.php");
+require_once "../Item/Item.php";
 
 class Inventario {
     private array $itens = []; 
     private float $capacidadeMaxima;
 
     public function __construct() {
-        $this->capacidadeMaxima = 20;
+        $this->capacidadeMaxima = 20;  
     }
 
     public function getCapacidadeMaxima(): float {
         return $this->capacidadeMaxima;
     }
-    public function adicionar(Item $itens): void {
-        array_push($this->itens, $itens);
+
+    public function adicionar(Item $item): void {
+        $tamanhoAtual = $this->calcularTamanhoAtual();
+        $tamanhoNovoItem = $item->getTamanho();
+
+        if ($tamanhoAtual + $tamanhoNovoItem <= $this->capacidadeMaxima) {
+            array_push($this->itens, $item);
+            echo "Item {$item->getNome()} adicionado com sucesso!<br>";
+        } else {
+            echo "Não é possível adicionar o item {$item->getNome()}. Capacidade máxima foi atingida <br>";
+        }
     }
+
     public function remover(string $nome): void {
         foreach ($this->itens as $indice => $item) {
             if ($item->getNome() === $nome) { 
@@ -25,11 +35,13 @@ class Inventario {
                 return; 
             }
         }
-        echo "Item ( {$nome} ) não encontrado no inventário.<br>";
+        echo "Item ( {$nome} ) não foi encontrado no inventário.<br>";
     }
+
     public function atualizarCapacidade(int $nivel): void {
         $this->capacidadeMaxima += ($nivel * 3);
     }
+
     public function calcularTamanhoAtual(): int {
         $tamanhoTotal = 0;
         foreach ($this->itens as $item) {
