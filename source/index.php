30f56b1306b9d218b65f0e9405671b7d3b275230
takiguchi <alexandre.pupim360@gmail.com>
2024-11-29 13:42:37 -0300

    Adicionando ao index o 'inventario.php'
    Adicionando as funções de chamada do 'Inventario.php' ao 'index.php'

--- source/index.php
+++ source/index.php
@@ -1,11 +1,35 @@
 <?php
-
-require_once "../Item/Item.php";
-require_once "../Item/Ataque.php";
-require_once "../Item/Defesa.php";
-require_once "../Item/Magia.php";
+require_once ("../Item/Item.php");
+require_once ("../Item/Ataque.php");
+require_once ("../Item/Defesa.php");
+require_once ("../Item/Magia.php");
+require_once ("../Player/Inventario.php");
 
 $espada = new Ataque("Espada");
 $escudo = new Defesa("Escudo");
 $feitiço = new Magia("Feitiço de Fogo");
+$machado = new Ataque("Machado");
+$capa = new Defesa("Capa Mágica");
+$feitiçoDeGelo = new Magia("Feitiço de Gelo");
 
+$inventarioPlayer1 = new Inventario();
+$inventarioPlayer2 = new Inventario();
+
+$inventarioPlayer1->adicionar($espada);
+$inventarioPlayer1->adicionar($escudo);
+$inventarioPlayer1->adicionar($feitiço);
+$inventarioPlayer1->adicionar($machado);
+$inventarioPlayer1->adicionar($capa);
+$inventarioPlayer1->adicionar($feitiçoDeGelo);
+
+$inventarioPlayer2->adicionar($espada);
+$inventarioPlayer2->adicionar($feitiço);
+$inventarioPlayer2->adicionar($machado);
+$inventarioPlayer2->adicionar($escudo);
+$inventarioPlayer2->adicionar($feitiçoDeGelo);
+
+$inventarioPlayer1->atualizarCapacidade(2); 
+
+$inventarioPlayer2->atualizarCapacidade(3);
+
+?>
